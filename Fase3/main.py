7245a709acd05dff1078be1428468d8f8c1e676f
WilliamMazariegos202100123 <williammazariegos6@gmail.com>
2024-12-28 17:00:29 -0600

    pruebas con traductor y bot

--- Fase3/main.py
+++ Fase3/main.py
@@ -1,97 +1,199 @@
 import tkinter as tk
-from idioma_chat import traducir
+from tkinter import scrolledtext
+from sklearn.feature_extraction.text import TfidfVectorizer  # Importar TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+import nltk
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import wordnet
+import unicodedata
+import re
+import pandas as pd
+from idioma_chat import detectar_lang, traducir
 
-def saludo():
-    chat_display.config(state=tk.NORMAL)
-    chat_display.insert(tk.END, "Bot: ¡Hola! ¿Cómo puedo ayudarte hoy?\n")
-    chat_display.config(state=tk.DISABLED)
+lang_envio = "en"
+
+# Cargar el archivo de preguntas y respuestas
+df = pd.read_csv("preguntas.csv", delimiter=";")
+
+# Descargar recursos necesarios de nltk
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+
+# Inicializar el Stemmer y el Lemmatizer
+stemmer = PorterStemmer()
+lemmatizer = WordNetLemmatizer()
+
+# Función para eliminar tildes
+def remove_accents(text):
+    nfkd_form = unicodedata.normalize('NFKD', text)
+    return ''.join([c for c in nfkd_form if not unicodedata.combining(c)])
+
+# Función para eliminar caracteres no alfabéticos
+def remove_non_alphabets(text):
+    return re.sub(r'[^a-záéíóúñ\s]', '', text)
+
+# Función para preprocesar el texto
+def preprocess_text(text):
+    text = text.lower()  # Convertir a minúsculas
+    text = remove_accents(text)  # Eliminar tildes
+    text = remove_non_alphabets(text)  # Eliminar caracteres no alfabéticos
     
-def chistes():
-    chat_display.config(state=tk.NORMAL)
-    chat_display.insert(tk.END, "Bot: ¿Por qué los programadores prefieren el osciloscopio? ¡Porque les gustan las señales!\n")
-    chat_display.config(state=tk.DISABLED)
+    tokens = nltk.word_tokenize(text)  # Tokenizar
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]  # Stemming
+    lemmatized_tokens = [lemmatizer.lemmatize(token, pos=wordnet.VERB) for token in stemmed_tokens]  # Lemmatización
+    return " ".join(lemmatized_tokens)
+
+# Preprocesar las preguntas
+print("Preprocesando las preguntas...")
+#X = [preprocess_text(text) for text in df['PREGUNTA']]  # Preprocesar preguntas
+X = df['PREGUNTA']  # Preguntas
+y = df['RESPUESTA']  # Respuestas
+print("Preprocesamiento completado.")
+
+# Convertir texto a características numéricas usando TF-IDF
+print("Convirtiendo las preguntas a características numéricas usando TF-IDF...")
+vectorizer = TfidfVectorizer(stop_words="english", max_features=1000, ngram_range=(1, 2))  # Configuración de TF-IDF
+X_vectors = vectorizer.fit_transform(X)
+print("Conversión a características numéricas completada.")
+
+# Entrenar el modelo
+print("Entrenando el modelo...")
+model = MultinomialNB(alpha=0.5) # se puede cambiar el valor de alpha o quitarlo
+model.fit(X_vectors, y)
+print("Modelo entrenado con éxito.")
+
+# Función para predecir la respuesta
+def get_response(user_input):
+    user_input_preprocessed = preprocess_text(user_input)
+    input_vector = vectorizer.transform([user_input_preprocessed])
     
-def datos_curiosos():
-    chat_display.config(state=tk.NORMAL)
-    chat_display.insert(tk.END, "Bot: Sabías que el primer bug fue un insecto real que quedó atrapado en un computador?\n")
-    chat_display.config(state=tk.DISABLED)
+    # Obtener las probabilidades para todas las clases
+    probabilities = model.predict_proba(input_vector)
     
-def consejos():
-    chat_display.config(state=tk.NORMAL)
-    chat_display.insert(tk.END, "Bot: Siempre haz un buen control de versiones para tu código. ¡Nunca sabes cuándo lo necesitarás!\n")
-    chat_display.config(state=tk.DISABLED)
+    # Obtener la probabilidad más alta y su clase correspondiente
+    max_probability = probabilities.max()
+    max_class_index = probabilities.argmax()
+    predicted_class = model.classes_[max_class_index]
     
-def herramientas():
-    chat_display.config(state=tk.NORMAL)
-    chat_display.insert(tk.END, "Bot: Algunas herramientas útiles son GitHub, VSCode y Postman.\n")
-    chat_display.config(state=tk.DISABLED)
+    # Mostrar probabilidad más alta y clase correspondiente
+    print(f"Probabilidad más alta: {max_probability:.2f} para la clase: {predicted_class}")
     
-def conceptos():
-    chat_display.config(state=tk.NORMAL)
-    chat_display.insert(tk.END, "Bot: Un concepto importante de programación es la encapsulación, que agrupa datos y métodos.\n")
-    chat_display.config(state=tk.DISABLED)
-    
-def enviar_mensaje():
-    user_message = message_input.get()
-    if user_message:
-        chat_display.config(state=tk.NORMAL)
-        chat_display.insert(tk.END, f"Tú: {user_message}\nTraduccion: {traducir(user_message)}\n") #importe la función traducir y la usé aqui para probar
-        message_input.delete(0, tk.END)
-        chat_display.config(state=tk.DISABLED)
+    """ # Devolver la predicción si supera el umbral
+    if max_probability < 0.30:
+        return "Lo siento, no entiendo tu pregunta." """
+    return predicted_class
+
+# Función para mostrar un mensaje en el área de texto
+def display_message(message, side):
+    message_area.config(state=tk.NORMAL)
+    if side == "left":
+        message_area.insert(tk.END, f"{message}\n", "left_message")
+    else:
+        message_area.insert(tk.END, f"{message}\n", "right_message")
+    message_area.yview(tk.END)
+    message_area.config(state=tk.DISABLED)
+
+# Función para enviar un mensaje
+def send_message(event=None):
+    if send_button['state'] == 'normal':
+        message = message_entry.get()
+        if message.strip():
+            display_message("👤: " + message+"  ", "right")
+            msj_bot=get_response(message)
+            l=detectar_lang(message)
+            if l!=lang_envio:
+                message=traducir(message)
+                msj_bot=get_response(message)
+
+            msj_final=msj_bot if l==lang_envio else traducir(msj_bot)
+            message_entry.delete(0, tk.END)
+            disable_send_button()
+            receive_message("🤖: " + msj_final)
+
+# Función para recibir un mensaje
+def receive_message(message):
+    message_area.config(bg="gray90")
+    typing_effect(message, 0)
+    vt=validar_texto(message)
+    print(vt)
 
 
-def iniciar_app():
-    intro_frame.pack_forget()  
-    menu_frame.pack(pady=10) 
-    chat_frame.pack(pady=10)  
+def validar_texto(texto):
+    # Lista para almacenar las partes del texto procesado
+    resultado = []
+    
+    # Utilizamos re.finditer para buscar partes dentro y fuera de '|'
+    partes = re.finditer(r'(\|([^|]+)\|)|([^|]+)', texto)
+    
+    # Procesamos cada coincidencia
+    for match in partes:
+        if match.group(2):  # Si el texto está entre '|', group(2) tendrá el texto entre las barras
+            resultado.append(f"☺{match.group(2).strip()}")
+        elif match.group(3):  # Si no está entre '|', group(3) tendrá el texto fuera de las barras
+            resultado.append(match.group(3).strip())
+
+    return resultado
+
+# Simular efecto de escritura con manejo de caracteres especiales
+def typing_effect(message, index):
+    # Reemplazar caracteres especiales por sus equivalentes visibles
+    formatted_message = message.replace("\\n", "\n").replace("\\t", "\t")
+    
+    if index < len(formatted_message):
+        message_area.config(state=tk.NORMAL)
+        message_area.insert(tk.END, formatted_message[index], "left_message")
+        message_area.config(state=tk.DISABLED)
+        message_area.yview(tk.END)
+        root.after(20, typing_effect, message, index + 1)
+    else:
+        message_area.config(state=tk.NORMAL)
+        message_area.insert(tk.END, "\n")
+        message_area.config(state=tk.DISABLED)
+        enable_send_button()
 
 
+# Habilitar/deshabilitar botón de enviar
+def disable_send_button():
+    send_button.config(state=tk.DISABLED)
+
+def enable_send_button():
+    send_button.config(state=tk.NORMAL)
+
+# Configurar la ventana principal
 root = tk.Tk()
 root.title("UsatBot")
-root.geometry("500x600")
+width, height = 700, 600
+x, y = (root.winfo_screenwidth() // 2 - width // 2, root.winfo_screenheight() // 2 - height // 2)
+root.geometry(f"{width}x{height}+{x}+{y}")
+root.resizable(False, False)
 
-intro_frame = tk.Frame(root)
-intro_frame.pack(pady=10)
-intro_img = tk.PhotoImage(file="images/logo.png")  
-intro_label = tk.Label(intro_frame, image=intro_img)
-intro_label.pack()
-welcome_label = tk.Label(intro_frame, text="¡Bienvenido al UsatBot!", font=("Arial", 16))
-welcome_label.pack()
+# Marco principal
+frame_main = tk.Frame(root)
+frame_main.pack(fill="both", expand=True)
 
+# Área de texto
+frame_messages = tk.Frame(frame_main)
+frame_messages.pack(padx=10, pady=10, fill="both", expand=True)
+message_area = scrolledtext.ScrolledText(frame_messages, wrap=tk.WORD, width=40, height=20, state=tk.DISABLED)
+message_area.pack(padx=10, pady=10, fill="both", expand=True)
+message_area.tag_configure("red", foreground="red", background="yellow")
 
-root.after(3000, iniciar_app)  
+# Entrada de texto
+frame_input = tk.Frame(frame_main)
+frame_input.pack(pady=10, fill="x", expand=True)
+message_entry = tk.Entry(frame_input, width=30, font=("Arial", 14))
+message_entry.pack(side=tk.LEFT, padx=50, fill="x", expand=True)
+message_entry.bind("<Return>", send_message)
+message_entry.focus()
 
-menu_frame = tk.Frame(root)
-menu_frame.pack_forget()  
-menu_label = tk.Label(menu_frame, text="Selecciona un tema:", font=("Arial", 14))
-menu_label.pack()
+# Botón enviar
+send_button = tk.Button(frame_input, text="Enviar", command=send_message, font=("Arial", 14))
+send_button.pack(side=tk.LEFT, padx=(0, 100))
 
-buttons = [
-    ("Saludos", saludo),
-    ("Chistes de programación", chistes),
-    ("Datos curiosos de programación", datos_curiosos),
-    ("Consejos de programación", consejos),
-    ("Herramientas útiles", herramientas),
-    ("Conceptos básicos de programación", conceptos)
-]
+# Configuración de mensajes
+message_area.tag_configure("left_message", justify="left", foreground="black", font=("Arial", 14, "bold"))
+message_area.tag_configure("right_message", justify="right", foreground="blue", font=("Arial", 14, "bold"))
 
-for text, func in buttons:
-    button = tk.Button(menu_frame, text=text, width=30, command=func)
-    button.pack(pady=5)
-
-
-chat_frame = tk.Frame(root)
-chat_frame.pack_forget()  
-chat_display = tk.Text(chat_frame, height=15, width=50, wrap=tk.WORD)
-chat_display.pack()
-chat_display.config(state=tk.DISABLED)
-
-message_input_frame = tk.Frame(root)
-message_input_frame.pack(pady=10)
-message_input = tk.Entry(message_input_frame, width=40)
-message_input.pack(side=tk.LEFT, padx=5)
-message_input.bind("<Return>", lambda event: enviar_mensaje())  # Vincula Enter con enviar_mensaje
-send_button = tk.Button(message_input_frame, text="Enviar", command=enviar_mensaje)
-send_button.pack(side=tk.LEFT)
-
+# Ejecutar la aplicación
 root.mainloop()
